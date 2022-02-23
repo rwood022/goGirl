ece8d35479c6b3937d76c270f7c1cc251b66eabd
DrMcCollum5 <94315015+DrMcCollum5@users.noreply.github.com>
2022-02-22 22:46:25 -0500

    installed react bootstrap, created css file, created header.js, created login.js updated app.js files

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Button } from "react-bootstrap";
+import Header from "./components/Header";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header />
+      <div className="login-buttons">
+        <Button variant="primary" className="signin-btn">
+          Sign In
+        </Button>
+        <Button variant="info" className="signup-btn">
+          Sign Up
+        </Button>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
