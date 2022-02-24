8e785efe7297bee79549b6a479a6211282fad443
rwood022 <93175265+rwood022@users.noreply.github.com>
2022-02-23 23:16:20 -0500

    added header.js

--- front_end/src/components/Header.js
+++ front_end/src/components/Header.js
@@ -1,7 +1,25 @@
 import React from "react";
+import { Link, useNavigate } from 'react-router-dom';
+import { AUTH_TOKEN } from '../constants';
 
 const Header = () => {
-  return <h1 className="title">Go Girl</h1>;
-};
+  const navigate = useNavigate();
+  const authToken = localStorage.getItem(AUTH_TOKEN);
+  return (
+    <div className="flex pa1 justify-between nowrap purple">
+      <div className="flex flex-fixed black">
+        <Link to="/" className="no-underline black">
+          <div className="fw7 mr1">goGirl</div>
+        </Link>           
+        <Link to="/" className="ml1 no-underline black">
+          new
+        </Link>
+        <div className="ml1">|</div>
+        <Link
+          to="/search"
+          className="ml1 no-underline black"
+        >
+      
+        
 
 export default Header;
