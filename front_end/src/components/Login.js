efa23c5874811e86aa85f09bd5292ad00fcc896e
rwood022 <93175265+rwood022@users.noreply.github.com>
2022-02-23 23:11:31 -0500

    added login

---
+++ front_end/src/components/Login.js
@@ -0,0 +1,137 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useMutation, gql } from '@apollo/client';
+import { AUTH_TOKEN } from '../constants';
+
+
+// takes arguments and return the token(makes requests on behalf of the user)
+const SIGNUP_MUTATION = gql`
+  mutation SignupMutation(
+    $email: String!
+    $password: String!
+    $name: String!
+  ) {
+    signup(
+      email: $email
+      password: $password
+      name: $name
+    ) {
+      token
+    }
+  }
+`;
+
+const LOGIN_MUTATION = gql`
+  mutation LoginMutation(
+    $email: String!
+    $password: String!
+  ) {
+    login(email: $email, password: $password) {
+      token
+    }
+  }
+`;
+// onClick event button uses a ternary (useMutation Hook) to call either login or signup
+// onCompleted callback sets the user's token in local storage and redirects them to the home page afterward
+const [login] = useMutation(LOGIN_MUTATION, {
+    variables: {
+      email: formState.email,
+      password: formState.password
+    },
+    onCompleted: ({ login }) => {
+      localStorage.setItem(AUTH_TOKEN, login.token);
+      navigate('/');
+    }
+  });
+  
+  const [signup] = useMutation(SIGNUP_MUTATION, {
+    variables: {
+      name: formState.name,
+      email: formState.email,
+      password: formState.password
+    },
+    onCompleted: ({ signup }) => {
+      localStorage.setItem(AUTH_TOKEN, signup.token);
+      navigate('/');
+    }
+  });
+
+
+//for users that already have an account
+const Login = () => {
+  const navigate = useNavigate();
+  const [formState, setFormState] = useState({
+    login: true,
+    email: '',
+    password: '',
+    name: ''
+  });
+  return (
+    <div>
+      <h4 className="mv3">
+        {formState.login ? 'Login' : 'Sign Up'}
+      </h4>
+      <div className="flex flex-column">
+        {!formState.login && (
+          <input
+            value={formState.name}
+            onChange={(e) =>
+              setFormState({
+                ...formState,
+                name: e.target.value
+              })
+            }
+            type="text"
+            placeholder="Your name"
+          />
+        )}
+        <input
+          value={formState.email}
+          onChange={(e) =>
+            setFormState({
+              ...formState,
+              email: e.target.value
+            })
+          }
+          type="text"
+          placeholder="Your email address"
+        />
+        <input
+          value={formState.password}
+          onChange={(e) =>
+            setFormState({
+              ...formState,
+              password: e.target.value
+            })
+          }
+          type="password"
+          placeholder="Choose a safe password"
+        />
+      </div>
+      <div className="flex mt3">
+        <button
+          className="pointer mr2 button"
+          onClick={formState.login ? login : signup}
+        //   onClick={() => console.log('onClick')}
+        >
+          {formState.login ? 'login' : 'create account'}
+        </button>
+        <button
+          className="pointer button"
+          onClick={(e) =>
+            setFormState({
+              ...formState,
+              login: !formState.login
+            })
+          }
+        >
+          {formState.login
+            ? 'need to create an account?'
+            : 'already have an account?'}
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
