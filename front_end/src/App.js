a578ddfa0e1d548a02e55a5637a3c5db154242da
rwood022 <93175265+rwood022@users.noreply.github.com>
2022-02-23 23:46:11 -0500

    added app.js for login

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,34 +1,55 @@
-import React from "react";
-import { Button } from "react-bootstrap";
-import { Redirect } from "react-router-dom";
-import Header from "./components/Header";
+import Login from './components/Login';
 
-const App = (props) => {
-  console.log(props);
-
-  const handleRedirect = (path) => {
-    props.history.push(path);
-  };
+const App = () => {
   return (
-    <div className="container">
-      <div className="login-buttons">
-        <Button
-          variant="primary"
-          className="signin-btn"
-          onClick={() => handleRedirect("/signin")}
-        >
-          Sign In
-        </Button>
-        <Button
-          variant="info"
-          className="signup-btn"
-          onClick={() => handleRedirect("/signup")}
-        >
-          Sign Up
-        </Button>
+    <div className="center w85">
+      <Header />
+      <div className="ph3 pv1 background-gray">
+        <Routes>
+          <Route path="/" element={<LinkList/>} />
+          <Route
+            path="/create"
+            element={<CreateLink/>}
+          />
+          <Route path="/login" element={<Login/>} />
+        </Routes>
       </div>
     </div>
   );
 };
+// import React from "react";
+// import { Button } from "react-bootstrap";
+// import { Redirect } from "react-router-dom";
+// import Header from "./components/Header";
+// import Login from './components/Login';
+
+// const App = (props) => {
+//   console.log(props);
+
+//   const handleRedirect = (path) => {
+//     props.history.push(path);
+//   };
+//   return (
+
+//     <div className="container">
+//       <div className="login-buttons">
+//         <Button
+//           variant="primary"
+//           className="signin-btn"
+//           onClick={() => handleRedirect("/signin")}
+//         >
+//           Sign In
+//         </Button>
+//         <Button
+//           variant="info"
+//           className="signup-btn"
+//           onClick={() => handleRedirect("/signup")}
+//         >
+//           Sign Up
+//         </Button>
+//       </div>
+//     </div>
+//   );
+// };
 
 export default App;
--- front_end/src/components/Header.js
+++ front_end/src/components/Header.js
@@ -19,7 +19,45 @@ const Header = () => {
           to="/search"
           className="ml1 no-underline black"
         >
-      
-        
+          search
+        </Link>
+        {authToken && (
+          <div className="flex">
+            <div className="ml1">|</div>
+            <Link
+              to="/create"
+              className="ml1 no-underline black"
+            >
+              submit
+            </Link>
+          </div>
+        )}
+      </div>
+      <div className="flex flex-fixed">
+        {authToken ? (
+          <div
+            className="ml1 pointer black"
+            onClick={() => {
+              localStorage.removeItem(AUTH_TOKEN);
+              navigate(`/`);
+            }}
+          >
+            logout
+          </div>
+        ) : (
+          <Link
+            to="/login"
+            className="ml1 no-underline black"
+          >
+            login
+          </Link>
+        )}
+      </div>
+    </div>
+  );
+};
+
+
 
 export default Header;
+    
