3e848c0fe72c82e068b7103d3fa68f6cbc377503
Stephanie Fajardo <stfajardo@gmail.com>
2022-03-01 15:39:25 -0500

    Revert "update styling for desktop media query"
    This reverts commit 36494e20d2ddda2c3a11086462f73ca273cabeb4.

--- front_end/src/components/Timeline.js
+++ front_end/src/components/Timeline.js
@@ -28,8 +28,7 @@ export default function Timeline() {
 }, []) 
 
     return (
-    <div>
-        <div className="timeline-container mx-auto"> 
+    <div> 
         <div className="post full-width">
             <div className="container card-body mb-2">
                 <h5 className="mb-0">{userData}</h5>
@@ -58,7 +57,6 @@ export default function Timeline() {
                 <strong>3</strong><small> Shares</small>
                 </a>
             </div>
-        </div>
         </div>
     </div>
     ); 
