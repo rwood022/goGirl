c7f132957cfb1522a04eea603f303a215a1085e7
Stephanie Fajardo <stfajardo@gmail.com>
2022-03-03 13:01:47 -0500

    add jungs code for timestamp

--- front_end/src/components/Timeline.js
+++ front_end/src/components/Timeline.js
@@ -1,9 +1,11 @@
 // import React from 'react';
 import { useState, useEffect } from 'react';
+import moment  from 'moment';
 
 export default function Timeline() {
 
     const [postData, setPostData] = useState([]);
+    const [timestamp, setTimestamp] = useState();
     // const [responseData, setResponseData] = useState([]);
     // const [timestamp, setTimestamp] = useState(['Timestamp']);
     // const [message, setMessage] = useState([]);
@@ -21,6 +23,31 @@ export default function Timeline() {
             .then((res) => res.json())
             .then((posts) => {
                 console.log(posts);
+          
+                // use moment.js to format the time with .fromNow
+                const postDate = moment().format(posts[0].createdAt);
+                let postDateValues = postDate.split("-")
+                if (postDateValues[2]) {
+                    let othervalue = postDateValues[2].split('T');
+                    postDateValues[2] = othervalue[0]
+                    postDateValues.pop();
+                }
+                // console.log (postDateValues);
+                let nowTime = moment().format('YYYY-MM-DD');
+                let fromNowValues = nowTime.split("-")
+                // console.log(fromNowValues)
+                // console.log(nowTime);
+                // console.log postDate);
+                // console.log(moment postDateValues).fromNow());
+                console.log(moment (postDateValues).from(moment(fromNowValues)))
+                setTimestamp(moment (postDateValues).from(moment(fromNowValues)));
+                // console.log(time);
+
+                // console.log(moment().posts[0].createdAt.fromNow())
+
+                // const timestamp = moment().startOf('day').fromNow();
+                // console.log(timestamp)
+
                 let postsArray = []; 
             // for(let i = 0; i < posts.length; i++) {
             //     setUserData(posts[i].username);
@@ -40,7 +67,6 @@ export default function Timeline() {
                 // setResponseData(posts[i].responses[0]);
             }
             
-
             })
             .catch(err => {
                 console.error(err);
@@ -49,11 +75,10 @@ export default function Timeline() {
 }, []); 
 
 
-
     return (
     <div> 
      {postData && postData.map((postData) => {
-         return  <div className="post full-width" key={postData._id}>
+         return  <div className="post full-width mt-4" key={postData._id}>
             <div className="container card-body mb-2">
                 <h5 className="mb-0">{postData.username}</h5>
                 <small>Posted at {postData.createdAt}</small>
