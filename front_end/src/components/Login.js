0c46df85408423b9a6a4f45e524dfe25e43ebbeb
rwood022 <93175265+rwood022@users.noreply.github.com>
2022-03-04 13:31:11 -0500

    added gapi library and login

---
+++ front_end/src/components/Login.js
@@ -0,0 +1,22 @@
+import { GoogleLogin } from 'react-google-login';
+
+
+const clientId = "181782094880-j468gjdl5lnthk3g0657ac450o4qrmb6.apps.googleusercontent.com";
+
+
+function Login() {
+
+    return(
+        <div id="signInButton">
+            <GoogleLogin
+                clientId={clientId}
+                buttonText="Login"
+                onSuccess={onSuccess}
+                onFailure={onFailure}
+                cookiePolicy={"single_host_origin"}
+                isSignedIn={true}
+            />
+        </div>
+    )
+
+}
