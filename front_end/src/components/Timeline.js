6d532825c81f6156be5bbb8cd6a468e66ad99b0e
Stephanie Fajardo <stfajardo@gmail.com>
2022-03-04 11:12:16 -0500

    fix timeline component with working code, moment added, createdAt displays correctly, and friend timeline component added. keias stylings also showing up correctly

--- front_end/src/components/Timeline.js
+++ front_end/src/components/Timeline.js
@@ -1,97 +1,112 @@
-// import React from 'react';
-import { useState, useEffect } from "react";
+import { useState, useEffect } from 'react';
+import moment  from 'moment';
 
 export default function Timeline(props) {
-  const [userData, setUserData] = useState("Username");
-  const [timestamp, setTimestamp] = useState("Timestamp");
-  const [message, setMessage] = useState("Message");
-  const [response, setResponse] = useState("Response");
 
-  useEffect(() => {
-    const { match } = props;
-    const { path = "/" } = match;
-    if (path === "/timeline") {
-      document.body.style.backgroundImage =
-        "linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), url(/images/timeline.jpg)";
-      document.body.style.backgroundSize = "cover";
-      document.body.style.backgroundPosition = "center";
-      document.body.style.height = "100vh"; // vh stands for viewport height
-      document.body.style.backgroundRepeat = "no-repeat";
-    }
-  }, []);
+    const [postData, setPostData] = useState([]);
+    const [timestamp, setTimestamp] = useState([]);
 
-  useEffect(() => {
-    fetch("http://localhost:3001/api/posts", {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(),
-    })
-      .then((res) => res.json())
-      .then((posts) => {
-        console.log(posts);
-        for (let i = 0; i < posts.length; i++) {
-          setUserData(posts[i].username);
-          setTimestamp(posts[i].createdAt);
-          setMessage(posts[i].message);
-          setResponse(posts[i].responses);
+    useEffect(() => {
+        const { match } = props;
+        const { path = "/" } = match;
+        if (path === "/timeline") {
+          document.body.style.backgroundImage =
+            "linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), url(/images/timeline.jpg)";
+          document.body.style.backgroundSize = "cover";
+          document.body.style.backgroundPosition = "center";
+          document.body.style.height = "100vh"; // vh stands for viewport height
+          document.body.style.backgroundRepeat = "no-repeat";
         }
-      })
-      .catch((err) => {
-        console.error(err);
-      });
-  }, []);
+      }, []);
 
-  return (
-    <div className="timeline">
-      {/* {userData.map((userData) => (  */}
-      <div className="post full-width">
-        <div className="container card-body mb-2">
-          <h5 className="mb-0">{userData}</h5>
-          <small>Posted at {timestamp}</small>
-        </div>
+    useEffect(() => { 
+        
+        fetch('http://localhost:3001/api/posts', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+              },
+              body: JSON.stringify(),
+            })
+            .then((res) => res.json())
+            .then((posts) => {
+                console.log(posts);
+          
+                let postsArray = []; 
+                let timestampArray = [];
+    
+            for(let i= 0; i < posts.length; i++) {
+                postsArray.push(posts[i]);
+                setPostData([...postsArray]);
 
-        <div className="card-body">
-          <div className="card-body">{message}</div>
-          <hr></hr>
-          <div className="card-body">
-            {response[0].username} <br></br>
-            <p>
-              <small>{response[0].createdAt}</small>
-            </p>
-            <p>{response[0].responseBody}</p>
-          </div>
-        </div>
+                // use moment.js to format the time with .fromNow
+                const postDate = moment().format(posts[i].createdAt);
+                let postDateValues = postDate.split("-")
+                if (postDateValues[2]) {
+                    let othervalue = postDateValues[2].split('T');
+                    postDateValues[2] = othervalue[0]
+                    postDateValues.pop();
+                }
 
-        <div className="card-footer">
-          <a href="/" className="d-inline-block response-icons">
-            <strong>23</strong>
-            <small> Likes</small>
-          </a>
-          <a href="/" className="d-inline-block response-icons">
-            <strong>12</strong>
-            <small> Comments</small>
-          </a>
-          <a href="/" className="d-inline-block response-icons">
-            <strong>3</strong>
-            <small> Shares</small>
-          </a>
+                let nowTime = moment().format('YYYY-MM-DD');
+                let fromNowValues = nowTime.split("-")
+                console.log(moment (postDateValues).from(moment(fromNowValues)))
+                timestampArray.push(moment (postDateValues).from(moment(fromNowValues)))                
+                setTimestamp([...timestampArray]);
+            }
+            
+            })
+            .catch(err => {
+                console.error(err);
+            }); 
+            
+}, []); 
+
+    return (
+    <div className="timeline"> 
+     {postData && postData.map((postData) => {
+         return  <div className="post full-width mt-4" key={postData._id}>
+            <div className="container card-body mb-2">
+                <h5 className="mb-0">{postData.username}</h5>
+                {timestamp.shift((time) => {
+                    return (
+                    <small>Posted {time}</small> )
+                })}
+                
+            </div>
+            
+            <div className="card-body">
+                <div className="card-body">
+                    {postData.message}
+                </div> 
+                <hr></hr>
+                <div className="card-body">
+                   {postData.responses.map((response) => {
+                       return (<div key={response._id}><p>{response.username}</p>
+                       <p><small>Responded at {response.createdAt} </small></p>
+                       <p>responsebody  {response.responseBody}</p>
+                       </div>
+                       )
+                    }) } <br></br>
+                   
+                </div>
+            </div>
+
+            <div className="card-footer">
+                <a href="/" className="d-inline-block text-muted response-icons">
+                    <strong>23</strong>
+                    <small >  Likes</small>
+                </a>
+                <a href="/" className="d-inline-block text-muted response-icons">
+                    <strong>12</strong><small>  Comments</small>
+                </a>
+                <a href="/" className="d-inline-block text-muted response-icons">
+                <strong>3</strong><small> Shares</small>
+                </a>
+            </div>
         </div>
-      </div>
-      {/* ))}  */}
+})}
     </div>
-  );
+    ); 
 }
-
-// const [data, setData] = useState([]);
-
-// useEffect(() => {
-
-// fetch('/api/users/').then(res => {
-//     if(res.ok) {
-//         return res.json()
-//     }
-// }).then(userData => { setData(userData)});
-
-// }, [])
+    
