727a3d12cf40b262dbc6a8be46dba8a40be1e99c
Stephanie Fajardo <stfajardo@gmail.com>
2022-03-04 18:11:37 -0500

    update the app router.js so that it only renders dashboard

--- front_end/src/router/AppRouter.js
+++ front_end/src/router/AppRouter.js
@@ -4,10 +4,6 @@ import App from "../App";
 import SignUp from "../components/SignUp";
 import SignIn from "../components/SignIn";
 import Header from "../components/Header";
-// import Timeline from "../components/DashboardComponents/Timeline";
-// import FriendTimeline from "../components/DashboardComponents/FriendTimeline";
-// import Profile from "../components/DashboardComponents/Profile";
-// import ProfileUpdate from "../components/DashboardComponents/ProfileUpdate";
 import Dashboard from "../components/Dashboard";
 
 const AppRouter = () => (
@@ -19,10 +15,6 @@ const AppRouter = () => (
         <Route component={SignIn} path="/signin" />
         <Route component={SignUp} path="/signup" />
         <Route component={Dashboard} path="/dashboard" />
-        {/* <Route component={Timeline} path="/timeline" /> */}
-        {/* <Route component={FriendTimeline} path="/friend_timeline" />
-        <Route component={Profile} path="/profile" />
-        <Route component={ProfileUpdate} path="/profile/profile_update" /> */}
       </Switch>
     </BrowserRouter>
   </div>
