be11287c3fd4beb90ef1044359c5473ebf7710b4
rwood022 <93175265+rwood022@users.noreply.github.com>
2022-03-04 13:57:51 -0500

    added login and logout to app.js

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,12 +1,28 @@
 import React, { useEffect } from "react";
+import"./App.css";
+import LoginButton from "./components/Login";
+import LogoutButton from "./components/LogOut";
 import { Button } from "react-bootstrap";
 import { Redirect } from "react-router-dom";
 import Header from "./components/Header";
+import { gapi } from 'gapi-script';
+
+const clientId = "181782094880-j468gjdl5lnthk3g0657ac450o4qrmb6.apps.googleusercontent.com"
 
 const App = (props) => {
   console.log({ props });
 
   useEffect(() => {
+
+    function start() {
+      gapi.client.init({
+        clientId: clientId,
+        scope: ""
+      })
+    };
+
+    gapi.load('client:auth2', start);
+
     const { match } = props;
     const { path = "/" } = match;
     if (path === "/") {
@@ -25,13 +41,16 @@ const App = (props) => {
   return (
     <div className="container">
       <div className="login-buttons">
-        <Button
+        {/* <Button
           variant="primary"
           className="signin-btn"
           onClick={() => handleRedirect("/signin")}
         >
           Sign In
-        </Button>
+        </Button> */}
+      <LoginButton/>
+      <LogoutButton/>
+
         <Button
           variant="info"
           className="signup-btn"
