063971a7b69a42d8d3eda8f0d4be812655ca2e8d
rwood022 <93175265+rwood022@users.noreply.github.com>
2022-03-05 23:36:05 -0500

    added googlecontroller and cookie-parser

--- front_end/src/components/Login.js
+++ front_end/src/components/Login.js
@@ -19,8 +19,38 @@ function Login() {
         console.log("Login Failed! res: ", res);
     }
 
+
+    const handleLogin = async googleData => {
+        const res = await fetch("/api/v1/auth/google", {
+            method: "POST",
+            body: JSON.stringify({
+            token: googleData.tokenId
+          }),
+          headers: {
+            "Content-Type": "application/json"
+          }
+        })
+        const data = await res.json()
+        // store returned user 
+      }
+
     return(
-        // <div id="signInButton">
+   
+        <GoogleLogin
+            clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
+            buttonText="Log in with Google"
+            onSuccess={handleLogin}
+            onFailure={handleLogin}
+            cookiePolicy={'single_host_origin'}
+        />
+
+    )
+
+
+}
+
+export default Login;
+     // <div id="signInButton">
         //     <GoogleLogin
         //         clientId={clientId}
         //         buttonText="Login"
@@ -30,16 +60,3 @@ function Login() {
         //         isSignedIn={true}
         //     />
         // </div>
-        <GoogleLogin
-            clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
-            buttonText="Log in with Google"
-            onSuccess={handleLogin}
-            onFailure={handleLogin}
-            cookiePolicy={'single_host_origin'}
-        />
-    )
-
-
-}
-
-export default Login;
