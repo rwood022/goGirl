f84a772f0829b301929607d88542efc087bcbdd7
rwood022 <93175265+rwood022@users.noreply.github.com>
2022-03-05 21:16:03 -0500

    added new google login

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,13 +1,13 @@
 const mongoose = require("mongoose")
 
 const userSchema = new mongoose.Schema({
-    username: String,
-    googleId: String,
-    secret: String,
+  
 });
 
-userSchema.plugin(passportLocalMongoose);
-userSchema.plugin(findOrCreate);
+
 
 const User = mongoose.model('User', userSchema)
+
+
+
 module.exports = User;
