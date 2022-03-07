d0bf3d4607cb3065265d75691adae56b39a5c26d
rwood022 <93175265+rwood022@users.noreply.github.com>
2022-03-07 11:02:26 -0500

    port works 3001 with oauth

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,27 +1,18 @@
 const { connect, connection } = require('mongoose');
-const session = require("express-session");
-const passport = require("passport");
-const passportLocalMongoose = require("passport-local-mongoose");
-const GoogleStrategy = require("passport-google-oauth20").Strategy;
-const findOrCreate = require("mongoose-findorcreate");
+// const session = require("express-session");
+// const passport = require("passport");
+// const passportLocalMongoose = require("passport-local-mongoose");
+// const GoogleStrategy = require("passport-google-oauth20").Strategy;
+// const findOrCreate = require("mongoose-findorcreate");
+
 
 //add new db
-
-app.use(session({
-  secret: "Our little secret.",
-  resave: false,
-  saveUninitialized: false
-}));
-app.use(passport.initialize());
-app.use(passport.session());
-
-
 connect('mongodb://localhost:27017/goGirl', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
-mongoose.set("useCreateIndex", true);
+
 
 
 
--- server/server.js
+++ server/server.js
@@ -1,24 +1,33 @@
+require('dotenv').config();
 const express = require('express');
 const db = require('./config/connection');
 const routes = require('./routes');
 const cors = require('cors');
-const mongoose = require('mongoose');
-// const session = require("express-session");
+// const mongoose = require('mongoose');
+const session = require("express-session");
 const passport = require("passport");
 // const passportLocalMongoose = require("passport-local-mongoose");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 // const findOrCreate = require("mongoose-findorcreate");
-
-
-
+const User = require('./models/User');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+
+app.use(session({
+  secret: "Our little secret.",
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);
+
 
 // logout option
 app.get("/logout", function(req, res){
@@ -37,7 +46,7 @@ passport.deserializeUser(function(id, done) {
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "http://localhost:4000/auth/google/callback",
+    callbackURL: "http://localhost:3001/auth/google/callback",
     userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
   },
   function(accessToken, refreshToken, profile, cb) {
